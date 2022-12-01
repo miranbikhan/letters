9ff5016e3e8824c3ba0480598e133386ebe8e12b
Ruben Schadron <schadronruben@hotmail.com>
2022-12-01 12:47:19 +0100

    new

--- R.py
+++ R.py
@@ -1,18 +1,16 @@
 import turtle
 t=turtle.Turtle()
 
-def drawn():
-    t.penup()
-    t.goto(-30,50) #centering in the screen
-    t.pendown()
+def draw():
     t.pensize(10)
     t.pencolor("red")
-    t.right(90)
-    t.forward(150)
-    t.goto(-30,50)
     t.right(-90)
+    t.forward(150)
+    t.right(90)
     t.circle(-40,180,10)
     t.penup()
-    t.left(135)
+    t.left(130)
     t.pendown()
-    t.forward(80)
+    t.forward(90)
+
+draw()
