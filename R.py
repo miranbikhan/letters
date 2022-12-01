1d7d313dc82f6af0c840fd3f8fc9f63ac51f1192
Ruben Schadron <schadronruben@hotmail.com>
2022-12-01 12:39:59 +0100

    .

--- R.py
+++ R.py
@@ -1,16 +1,18 @@
 import turtle
 t=turtle.Turtle()
-t.penup()
-t.goto(-30,50) #centering in the screen
-t.pendown()
-t.pensize(10)
-t.pencolor("red")
-t.right(90)
-t.forward(150)
-t.goto(-30,50)
-t.right(-90)
-t.circle(-40,180,10)
-t.penup()
-t.left(135)
-t.pendown()
-t.forward(80)
+
+def R():
+    t.penup()
+    t.goto(-30,50) #centering in the screen
+    t.pendown()
+    t.pensize(10)
+    t.pencolor("red")
+    t.right(90)
+    t.forward(150)
+    t.goto(-30,50)
+    t.right(-90)
+    t.circle(-40,180,10)
+    t.penup()
+    t.left(135)
+    t.pendown()
+    t.forward(80)
