50e20241a500f143733dd52f79331a8720b6f4b3
Ruben Schadron <schadronruben@hotmail.com>
2022-12-01 12:59:32 +0100

    Added letter U

---
+++ U.py
@@ -0,0 +1,12 @@
+import turtle
+t=turtle.Turtle()
+
+def draw():
+    t.pensize(10)
+    t.pencolor("red")
+    t.right(90)
+    t.forward(100)
+    t.circle(50,180,100)
+    t.forward(100)
+
+draw()
